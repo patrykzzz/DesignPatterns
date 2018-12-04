94b86d0442cba9cb1febdde5e0a6e8895581aca4
Patryk Miłek <patrykmilek96@gmail.com>
2018-12-04 18:54:47 +0100

    Implement thread-safe singleton

---
+++ 01Singleton/Students/2018/MilekPatryk/Singleton/Implementation/Singleton.cs
@@ -0,0 +1,31 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private static readonly object Lock = new object();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (Lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
---
+++ 01Singleton/Students/2018/MilekPatryk/Singleton/Tests/SingletonTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Singleton.Tests
+{
+    public class SingletonTests
+    {
+        [Fact]
+        public void GetInstance_ForThousandCopies_ShouldHaveSameInstance()
+        {
+            // Arrange
+            var instances = new Singleton[10];
+
+            // Act
+            Parallel.For(0, 10, index => instances[index] = Singleton.GetInstance());
+
+            // Assert
+            var expectedInstance = Singleton.GetInstance();
+            Assert.All(instances, instance => Assert.Same(expectedInstance, instance));
+        }
+
+        [Fact]
+        public void GetInstance_ForTwoCopies_ShouldHaveSameInstance()
+        {
+            // Arrange & Act
+            var firstInstance = Singleton.GetInstance();
+            var secondInstance = Singleton.GetInstance();
+            
+            // Assert
+            Assert.Same(firstInstance, secondInstance);
+        }
+    }
+}
